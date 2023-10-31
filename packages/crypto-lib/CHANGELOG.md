1adea77d6e806f3c76bfde4d4542ab045f82bb78
minmin.yan <minmin.yan@okg.com>
2023-10-31 14:24:25 +0800

    fromBech32 method add limit

---
+++ packages/crypto-lib/CHANGELOG.md
@@ -0,0 +1,10 @@
+
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+# [1.0.1](https://github.com/okx/js-wallet-sdk) (2023-10-31)
+
+### Bug Fixes
+
+- **crypto-lib:** fromBech32 method add limit
--- packages/crypto-lib/src/base/bech32.ts
+++ packages/crypto-lib/src/base/bech32.ts
@@ -6,8 +6,8 @@ export function toBech32(prefix: string, data: Buffer | Uint8Array | Number[]): 
     return bech32.encode(prefix, bit5)
 }
 
-export function fromBech32(data: string): [string, Buffer] {
-    const d = bech32.decode(data)
+export function fromBech32(data: string, limit?: number | false): [string, Buffer] {
+    const d = bech32.decode(data, limit)
     const bit8 = bech32.fromWords(d.words)
     return [d.prefix, Buffer.from(bit8)]
 }
