df041ebb9e90fa6fbee735eec0c32d10ac982e10
jinguang.huang <jinguang.huang@okg.com>
2023-11-17 16:02:03 +0800

    update kaspa address validate

--- packages/coin-kaspa/tests/kaspa.test.ts
+++ packages/coin-kaspa/tests/kaspa.test.ts
@@ -1,18 +1,17 @@
-import { Address } from "@kaspa/core-lib"
-import { KaspaWallet } from "../src/KaspaWallet";
+import { KaspaWallet, addressFromPubKey } from "../src";
 
 const wallet = new KaspaWallet();
 
 describe("kaspa", () => {
     test("address", async () => {
-        let isValid = await wallet.validAddress({ address: "kaspa:qrcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" });
-        console.log(isValid);
+        expect(await wallet.validAddress({ address: "kaspa:qrcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" })).toBe(true);
+        expect(await wallet.validAddress({ address: "kaspa:qrcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2a" })).toBe(false);
+        expect(await wallet.validAddress({ address: "kaspa:prcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" })).toBe(false);
+        expect(await wallet.validAddress({ address: "kaspa1:qrcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" })).toBe(false);
+        expect(await wallet.validAddress({ address: "kaspa:1prcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" })).toBe(false);
+        expect(await wallet.validAddress({ address: "kaspa:rcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" })).toBe(false);
 
-        isValid = await wallet.validAddress({ address: "kaspa:prcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h" });
-        console.log(isValid);
-
-        // @ts-ignore
-        const address = new Address(Buffer.from("f13b0d6390568bac735b080b03a993f492afbb9bc330333fdad4cd8b47206a80", "hex"), "kaspa").toString();
-        console.log(address);
+        const address = addressFromPubKey("f13b0d6390568bac735b080b03a993f492afbb9bc330333fdad4cd8b47206a80");
+        expect(address).toBe("kaspa:qrcnkrtrjptghtrntvyqkqafj06f9tamn0pnqvelmt2vmz68yp4gqj5lnal2h");
     });
 });
