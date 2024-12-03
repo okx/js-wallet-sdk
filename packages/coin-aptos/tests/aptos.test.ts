d1802452bc52a36b5bc63125f1580f9fd45b7d73
shuaishuai.si <shuaishuai.si@okg.com>
2024-12-03 11:41:46 +0800

    support aptos transfer_coins

--- packages/coin-aptos/tests/aptos.test.ts
+++ packages/coin-aptos/tests/aptos.test.ts
@@ -117,6 +117,35 @@ describe("aptos", () => {
         expect(base.toHex(data)).toBe(expected)
     });
 
+   //test in mainnet https://explorer.aptoslabs.com/txn/1999229005?network=mainnet
+    test("tokenTransferV2", async () => {
+        let wallet = new AptosWallet()
+        const param: AptosParam = {
+            type: "tokenTransferV2",
+            base: {
+                sequenceNumber: "2",
+                chainId: 27,
+                maxGasAmount: "200000",
+                gasUnitPrice: "100",
+                expirationTimestampSecs: "1722391521",
+            },
+            data: {
+                tyArg: "0x275f508689de8756169d1ee02d889c777de1cebda3a7bbcce63ba8a27c563c6f::tokens::USDC",
+                recipientAddress: "0x259523c5dc9415e85d30d6f4109ebba32984df5ddc72707f9a863834291332e9",
+                amount: 800,
+            }
+        }
+        let signParams: SignTxParams = {
+            privateKey: "3c94d9e6f0207a2659b9a118d74f512b399b87bf468baf53e11938f7a34442f1",
+            data: param
+        };
+        let tx = await wallet.signTransaction(signParams);
+        const expected = "8857b69756dfef4cd27c8e4547fc7dc36ceba177d2c35e23e853c45fda41e16d02000000000000000200000000000000000000000000000000000000000000000000000000000000010d6170746f735f6163636f756e740e7472616e736665725f636f696e730107275f508689de8756169d1ee02d889c777de1cebda3a7bbcce63ba8a27c563c6f06746f6b656e730455534443000220259523c5dc9415e85d30d6f4109ebba32984df5ddc72707f9a863834291332e9082003000000000000400d0300000000006400000000000000e19ba966000000001b00209d351559f1a1f703a73aa50474354a893ec58a08af2896dd3700fd2e97025c274025864c4a15d8fdf8f9e31aa0c5076399f7c3a8cde81e918d72c454bd4a7b047ab285888e535186363504aa7eb4c2e17acae073595014dd091c8b56ff9bdae103"
+        // https://blue.explorer.movementlabs.xyz/txn/0x18a0595993b264158c69bc8f63793c5ef11e2149e05f6e9339a87be9eb5703d6?network=testnet
+        expect(tx).toBe(expected)
+    });
+
+
     test("simulate", async () => {
         const account1 = new AptosAccount(base.fromHex("0x4a6d287353203941768551f66446d5d4a85ab685b5b444041801014ae39419b5067aec3603bdca82e52a172ec69b2505a979f1d935a59409bacae5c7f268fc26"))
         const expirationTimestampSecs = Math.ceil(Date.now() / 1000) + 300
